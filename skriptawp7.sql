6ea706ace75fb092b028adcc58e93fceeb94ad43
Zeljko Lucan <lucko1987vk@gmail.com>
2024-11-25 21:34:58 +0100

    Kreiranje tablice

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,1 +1,10 @@
-﻿create database edunovawp7;
+﻿-- ovako se pisu komentari
+
+-- ova naredba kreira bazu podataka
+--create database edunovawp7;
+
+-- use edunovawp7;
+
+create table smjer (
+naziv varchar (50)
+);
