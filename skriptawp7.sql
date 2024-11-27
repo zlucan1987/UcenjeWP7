019ab2f50491c69cc97e58fe8346360a92a28d62
Željko Lučan <lucko1987vk@gmail.com>
2024-11-27 20:18:42 +0100

    kreiranje tablice smjerovi

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -9,10 +9,11 @@ go
 use edunovawp7;
 go
 
---create database edunovawp7;
-
--- use edunovawp7;
-
-create table smjer (
-naziv varchar (50)
+create table smjerovi (
+sifra int,
+naziv varchar(50),
+trajanje int, 
+cijena decimal(18,2),
+vaucer bit, 
+izvodiseod datetime
 );
