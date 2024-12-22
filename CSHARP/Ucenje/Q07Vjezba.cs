b784671322896825c8e52dd055c96143e5baf453
Zeljko Lucan <lucko1987vk@gmail.com>
2024-12-22 22:38:39 +0100

    Vjezba zadataka

---
+++ CSHARP/Ucenje/Q07Vjezba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class Q07Vjezba
+    {
+
+        public static void Izvedi()
+        {
+
+            int i = 2, j = 3;
+            int k = i + j;
+            k = i - j;
+            k = i * j;
+            double t = k / j;
+            bool b = k == j;
+            b = k > j;
+            b = k != j;
+            bool p = true; b = false;
+            b = p & b;
+            b = p && b;
+
+            Console.WriteLine("i={0}, k={1}", i, k);
+
+        }
+
+    }
+}
---
+++ CSHARP/Ucenje/Q08Vjezba.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class Q08Vjezba
+    {
+        public static void Izvedi()
+        {
+            // 10 puta jedno ispod drugog ispišite Osijek
+            // nije dobro riješenje
+
+            // unaprijed
+            for (int i = 0; i < 10; i++)
+            {
+             
+                Console.WriteLine("{0}. Osijek iz petlje", i);
+
+            }
+
+            // unazad
+            for (int i = 0; i > 0; i--)
+            {
+
+                Console.WriteLine("{0}. Unazad", i);
+
+            }
+
+        }
+    }
+}
