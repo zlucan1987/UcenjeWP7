7b2226c77d91052c5f78f0d859019a576d5a60c8
Zeljko Lucan <lucko1987vk@gmail.com>
2024-12-23 02:12:17 +0100

    OP number 9 Vjezba

---
+++ CSHARP/Ucenje/Q09Vjezba.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ucenje
+{
+    internal class Q09Vjezba
+    {
+
+        public static void Main(string[] args)
+        {
+
+            static int ZbrojZnamenki(int broj)
+            {
+                int zbroj = 0;
+                while (broj > 0)
+                {
+                    zbroj += broj % 10;
+                    broj /= 10;
+                }
+                return zbroj;
+            }
+            Console.Write("Unesite cijeli broj: ");
+            if (int.TryParse(Console.ReadLine(), out int broj))
+            {
+                int rezultat = broj * 9;
+                Console.WriteLine("Rezultat množenja s 9 je: " + rezultat);
+
+                int zbrojZnamenki = ZbrojZnamenki(rezultat);
+                Console.WriteLine("Zbroj znamenki rezultata je: " + zbrojZnamenki);
+
+                while (zbrojZnamenki > 9)
+                {
+                    zbrojZnamenki = ZbrojZnamenki(zbrojZnamenki);
+                }
+
+                Console.WriteLine("Konačni zbroj znamenki je: " + zbrojZnamenki);
+            }
+            else
+            {
+                Console.WriteLine("Niste unijeli cijeli broj.");
+            }
+        }
+    }
+}
